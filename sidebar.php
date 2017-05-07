380e696e99fb5fd5ce3c6db3db5cef2ed72b3cf8
Alessandro Cuoghi <agileontheweb@gmail.com>
2017-05-07 15:22:42 +0200

    commit [#4]

--- sidebar.php
+++ sidebar.php
@@ -4,27 +4,27 @@
  *
  * @package sparkling
  */
-?>	 
+?>
 </div><!-- WTF? find left this div close.... -->
 <?
 $corsoPhotoshopAbility= get_post_meta($post->ID,  'photoshop-ability', true);
 $corsoIllustratorBase= get_post_meta($post->ID,  'illustrator-base', true);
- 
+
 ?>
 
 
-<div id="secondary" class="widget-area col-md-4 col-xs-12" role="complementary"> 
+<div id="secondary" class="widget-area col-md-4 col-xs-12" role="complementary">
 <?#php wp_nav_menu( array( 'theme_location' => 'photoshop-menu' ) ); ?>
     <div class="top-fixed  hidden-xs hidden-sm">
       <aside id="#newsletter-sidebar" class="widget">
         <div id="newsletter" class="newsletter text-center">
-          <img class=" img-circle"  src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/alle.png" alt="" width="100" height="100" /> 
+          <img class=" img-circle"  src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/alle.png" alt="" width="100" height="100" />
           <p>Sono Alessandro l' autore di Corsi di grafica: il blog in Italiano per grafici inesperti, ti do il benvenuto, qui troverai tecniche, esercizi, corsi e abilità di grafica digitale. </p>
           <h3><strong>Ricevi gratis</strong><br>Video lezione Illustrator + Guida Photoshop + Risorse + Bonus Extra</h3>
           <a href="" class="btn btn-primary btn-lg btn-block call-to-action" data-toggle="modal" data-target="#modal">Iscriviti</a>
         </div>
       </aside>
-       
+
       <?php if ( is_page_template('category-illustrator.php') ||  has_tag('illustrator-base') || ( $corsoIllustratorBase == "illustrator-base" ) ) {?>
         <aside class="widget ">
           <div class="illustrator-base-course">
@@ -36,7 +36,7 @@ $corsoIllustratorBase= get_post_meta($post->ID,  'illustrator-base', true);
           </div>
         </aside>
         <?php  ?>
-         
+
         <?php } if (is_page_template('category-photoshop.php') || has_tag('photoshop-ability') || ( $corsoPhotoshopAbility == "photoshop-ability" )) {?>
           <aside class="widget">
             <div class="photoshop-course">
@@ -44,9 +44,9 @@ $corsoIllustratorBase= get_post_meta($post->ID,  'illustrator-base', true);
               <h4>Un corso dedicato a chi vuole imparare Photoshop da zero</h4>
               <a href="http://www.corsidigrafica.info/photoshop-ability/" class="btn btn-default">Scopri di più</a>
             </div>
-          </aside> 
-            
-          <?}if(has_tag('illustrator-facile')) {?> 
+          </aside>
+
+          <?}if(has_tag('illustrator-facile')) {?>
           <aside class="widget2">
             <div class="illustrator-facile-course">
               <h3>Illustrator facile</h3>
@@ -54,9 +54,9 @@ $corsoIllustratorBase= get_post_meta($post->ID,  'illustrator-base', true);
               <!-- <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/mouse.png"> -->
               <a href="http://www.corsidigrafica.info/corso-illustrator-facile/" class="btn btn-default">Scopri di più</a>
             </div>
-          </aside> 
-           <?php } if(has_tag('fotolia')) {?> 
-           <aside class="widget2">
+          </aside>
+           <?php } if(has_tag('fotolia')) {?>
+           <!-- <aside class="widget2">
             <div class="fotolia">
               <h3>Fotolia</h3>
               <p>disegnare con il mouse</p>
@@ -64,6 +64,6 @@ $corsoIllustratorBase= get_post_meta($post->ID,  'illustrator-base', true);
               <!-- <h4>Realizza biglietti da visita, poster, stampe di grande formato, pieghevoli e materiale cartaceo in generale e scopri come creare la grafica di una pagina web.</h4> -->
               <a href="http://corsidigrafica.info/risorse/fotolia" class="btn btn-xs btn-default">Scopri di più</a>
             </div>
-          </aside>
+          </aside> -->
           <?}?>
 </div>
